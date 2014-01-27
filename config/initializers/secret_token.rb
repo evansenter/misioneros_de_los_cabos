80a21f5425bb5eb157f43887f28476c4132bddc8
Evan Senter <evansenter@gmail.com>
2014-01-27 15:50:11 -0500

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MisionesDelCabo::Application.config.secret_key_base = 'a32be6a8af80605c26f5159b60b45f9ccacc134f7260c0f6c5b159c74e9e6fc2558c3b8110b91887a6831731890698aad9047e6f2e0b11e3b310c3ec912f9e6b'
