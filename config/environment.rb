828240715e92b960cb837eafb24079bd5dee44b6
Evan Senter <evansenter@gmail.com>
2014-02-09 22:32:31 -0500

    Play nice now.

--- config/environment.rb
+++ config/environment.rb
@@ -10,8 +10,8 @@ Pony.options = {
     :address => 'smtp.sendgrid.net',
     :port => '587',
     :domain => 'heroku.com',
-    :user_name => Figaro.env.sendgrid_username,
-    :password => Figaro.env.sendgrid_password,
+    :user_name => ENV["SENDGRID_USERNAME"],
+    :password => ENV["SENDGRID_PASSWORD"],
     :authentication => :plain,
     :enable_starttls_auto => true
   }
